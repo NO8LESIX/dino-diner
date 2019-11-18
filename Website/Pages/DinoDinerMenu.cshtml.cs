0f525a49e1f37222ca2de144de9fa4c64b0ccbb5
Carlos Cerna <cbcerna@ksu.edu>
2019-11-18 15:26:08 -0600

    Near completion, but need to call values to fill in cards

--- Website/Pages/DinoDinerMenu.cshtml.cs
+++ Website/Pages/DinoDinerMenu.cshtml.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+
 
 namespace Website.Pages
 {
     public class DinoDinerMenuModel : PageModel
     {
+
+        Menu menu;
+
         public void OnGet()
         {
-
+            menu = new Menu();
         }
     }
 }
