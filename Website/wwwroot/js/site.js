ea524ce8b2c3947c26834eed9c40f8c4c24e81fd
Carlos Cerna <cbcerna@ksu.edu>
2019-12-02 15:17:43 -0600

    Started Navbar Development and fixed the container. Still needs to implement filter.

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+/*
+ * Side-nav overlay experiment code 
+ * https://www.w3schools.com/howto/howto_js_sidenav.asp
+ */
+
+//opens and resizes objects from the left of the screen
+function openNav() {
+    document.getElementById("Sidenav").style.width = "250px";
+    document.getElementById("content").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+//closes navbar to the left of the screen
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+    document.getElementById("content").style.width = "1fr";
+    document.body.style.backgroundColor = "white";
+}
+
+//end Side-nav overlay experiment
